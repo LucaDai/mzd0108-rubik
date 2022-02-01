44752b7df52903acac342f99421e7e9744a0d854
fff19 <fff19@172.17.106.248>
2022-01-31 20:33:18 -0600

    COMP5700 Assignment 2
    _check.py: asserting validated cube.
    checkTest: developing BVA confidence level unit test cases.

--- rubik/check.py
+++ rubik/check.py
@@ -1,3 +1,9 @@
+"""
+Jan 31, 2022
+
+@author: Luca Dai
+"""
+
 import rubik.cube as rubik
 #Return {'status': 'ok'} if pass all conditions 
 #Return {'status': 'error: xxx'} where 'xxx' is an appropriate error message 
@@ -7,14 +13,19 @@ def _check(parms):
     encodedCube = parms.get('cube',None) 
     if (encodedCube == None):
         result['status'] = 'error: xxx'
+        
     #Value of cube is a string
-    elif (isinstance(encodedCube, str) == False):
-        result['status'] = 'error: xxx'
+    if not result:
+        if (isinstance(encodedCube, str) == False):
+            result['status'] = 'error: xxx'
+        
     #Value of cube has 54 elements
-    elif (len(encodedCube) != 54):
-        result['status'] = 'error: xxx'
+    if not result:
+        if (len(encodedCube) != 54):
+            result['status'] = 'error: xxx'
+        
     #Value of cube has 9 occurrences of 6 colors
-    elif not result:
+    if not result:
         color_dict = {}
         for element in range(0, len(encodedCube)):
             if (encodedCube[element] in color_dict):
@@ -29,22 +40,70 @@ def _check(parms):
             if (color_dict[element] != 9):
                 result['status'] = 'error: xxx' 
                 break
+            
     #Value of cube has each middle face being a different color
-    elif not result:
+    if not result:
     #Middle face start from number 5(index 4), and increasing 9 every time    
-        mid_list = []
+        mid_color = []
         mid_face = 4
         for element in range(6):
-            if(encodedCube[mid_face] in mid_list):
+            if(encodedCube[mid_face] in mid_color):
                 result['status'] = 'error: xxx' 
                 break 
-            mid_list.append(encodedCube[mid_face])
-            mid_face += 9
-        
+            mid_color.append(encodedCube[mid_face])
+            mid_face += 9   
+            
     #Extra: Value of cube does not has contradictory color
-    #elif not result:
-        
+    if not result:    
+        #Insert index number of 12 edges
+        edge = [[2, 44], [4, 33], [6, 13], [8, 47], [11, 42], [15, 22],
+                [17, 51], [20, 38], [24, 31], [26, 53], [29, 40], [35, 48]]
+        #Transfer index in to color
+        edge_color = []
+        for element in edge:
+            ec1 = encodedCube[element[0] - 1]
+            ec2 = encodedCube[element[1] - 1]
+            edge_color.append([ec1, ec2])
+        #Insert index number of 12 edges
+        corner = [[1, 30, 43], [3, 10, 45], [7, 36, 46], [9, 16, 48],
+                  [12, 19, 39], [18, 25, 54], [21, 37, 28], [27, 34, 52]]
+        #Transfer index in to color
+        corner_color = []
+        for element in corner:
+            cc1 = encodedCube[element[0] - 1]
+            cc2 = encodedCube[element[1] - 1]
+            cc3 = encodedCube[element[2] - 1]
+            corner_color.append([cc1, cc2, cc3])
+        #Get all middle face color in a list
+        mid_color = []
+        mid_face = 4
+        for element in range(6):
+            mid_color.append(encodedCube[mid_face])
+            mid_face += 9
+        #list position: cube index
+        #1:5, 2:14, 3: 23, 4:32, 5:41, 6:50
+        #Face 5 against 23, 14 against 32, 41 against 50
+        #In order to use loop, swap position 2 and 3
+        #color contradictory: x with x+1
+        mid_color[1], mid_color[2] = mid_color[2], mid_color[1]
+        #Ready to check the color contradictory
+        #Check edges color contradictory
+        for element in edge_color:
+            for x in range(0, 6, 2):
+                front = mid_color[x]
+                back = mid_color[x+1]
+                if (front in element) and (back in element):
+                    result['status'] = 'error: xxx' 
+                    break
+        #Check corners color contradictory
+        for element in corner_color:
+            for x in range(0, 6, 2):
+                front = mid_color[x]
+                back = mid_color[x+1]
+                if (front in element) and (back in element):
+                    result['status'] = 'error: xxx' 
+                    break
     #The cube string fits all conditions
-    else:
+    if not result:
         result['status'] = 'ok'
     return result
--- rubik/test/checkTest.py
+++ rubik/test/checkTest.py
@@ -1,13 +1,211 @@
+"""
+Jan 31, 2022
+
+@author: Luca Dai
+"""
+
 from unittest import TestCase
 import rubik.check as check 
 
 class CheckTest(TestCase):
-        
-    def test_check_010_ShouldReturnOkOnSolvedCube(self):
+
+#Analysis
+#    _check(parm)
+#    
+#    inputs:
+#        parm:     dictionary, 2 keys, mandatory, unvalidated
+#            keys: 
+#                'op', string, mandatory, validated
+#                'cube',string, 54 characters,
+#                       has 9 occurrences of 6 colors,
+#                       each middle face has different color,
+#                       not having contradictory colors,
+#                       mandatory, unvalidated
+#    outputs:
+#        side-effects:    no state change
+#        returns:    dictionary, 1 key, one of the follow:
+#                    fail: {'status': 'error: xxx'} or pass: {'status': 'ok'}
+#
+#    confidence level:    BVA
+#
+#Happy path
+#    test 101:    the 'cube' value of parm is represent a solved cube:
+#                 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'
+#                 result: {'status': 'ok'}
+#    test 102:    the 'cube' value of parm is represent an unsolved but perfect cube:
+#                 'gybrbgggoobgowwwyboboggryorbbwwygwyyywyorrrbwrygroobwr'
+#                 result: {'status': 'ok'}
+#    test 103:    the 'cube' value of parm is represent an unsolvable corner rotated cube:
+#                 'bbrbbbbbbyrrrrrrrrgggggggggoooooooooyyyyyyyybwwwwwwwww'
+#                 result: {'status': 'ok'}
+#
+#Sad path
+#    test 901:    Key 'cube' does not exist: parm = {'op':'check'}
+#                 result: {'status': 'error: xxx'}
+#    test 902:    Value of cube is not string: 'cube' is integer
+#                 result: {'status': 'error: xxx'}
+#    test 903:    Bottom case of parm: string length is 0, 'cube' = ''
+#                 result: {'status': 'error: xxx'}
+#    test 904:    Less than low bound: length of 'cube' is 53
+#                 result: {'status': 'error: xxx'}
+#    test 905:    Greater than high bound: length of 'cube' is 55
+#                 result: {'status': 'error: xxx'}
+#    test 906:    Very big value: length of 'cube' is 108
+#                 result: {'status': 'error: xxx'}
+#    test 907:    Less than low bound: 'cube' only has 5 colors
+#                 result: {'status': 'error: xxx'}
+#    test 908:    Greater than high bound: 'cube' has 7 colors
+#                 result: {'status': 'error: xxx'}
+#    test 909:    Beginning of the String： Value of cube has different occurrences of 6 colors: 10 'b's and 8 'r's
+#                 result: {'status': 'error: xxx'}
+#    test 910:    End of string:： Value of cube has different occurrences of 6 colors: 8 'b's and 10 'w's
+#                 result: {'status': 'error: xxx'}
+#    test 911:    Beginning of the String： Value of cube has middle face being same color: [r,r,g,o,y,w]
+#                 result: {'status': 'error: xxx'}
+#    test 912:    End of the String： Value of cube has middle face being same color: [b,r,g,o,y,b] 
+#                 result: {'status': 'error: xxx'}
+#    test 913:    Edge case: one edge has contradictory color: 
+#                 swap position 20 and 44 for solved cube
+#                 result: {'status': 'error: xxx'}
+#    test 914:    Corner case: one corner has contradictory color:
+#                 swap position 19 and 43 for solved cube
+#                 result: {'status': 'error: xxx'}
+
+    #Happy path tests:
+    def test_check_101_ShouldReturnOkOnSolvedCube(self):
         parm = {'op':'check',
                 'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
         result = check._check(parm)
         self.assertIn('status', result)
         status = result.get('status', None)
         self.assertEqual(status, 'ok')
+        
+    def test_check_102_ShouldReturnOkOnUnsolvedCube(self):
+        parm = {'op':'check',
+                'cube':'gybrbgggoobgowwwyboboggryorbbwwygwyyywyorrrbwrygroobwr'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'ok')
+    def test_check_103_ShouldReturnOkOnUnsolvedCube(self):
+        parm = {'op':'check',
+                'cube':'bbrbbbbbbyrrrrrrrrgggggggggoooooooooyyyyyyyybwwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'ok')
+        
+        
+    #Sad path tests:    
+    def test_check_901_ShouldReturnXxxForCubeNotExist(self):
+        parm = {'op':'check'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx')
+        
+    def test_check_902_ShouldReturnXxxForNotString(self): 
+        parm = {'op':'check',
+                'cube': 111111111222222222333333333444444444555555555666666666}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx')
+        
+    def test_check_903_ShouldReturnXxxForEmptyString(self): 
+        parm = {'op':'check',
+                'cube':''}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx') 
+        
+    def test_check_904_ShouldReturnXxxForCubelengthOf53(self): 
+        parm = {'op':'check',
+                'cube':'bbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx') 
+        
+    def test_check_905_ShouldReturnXxxForCubelengthOf55(self): 
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx') 
+        
+    def test_check_906_ShouldReturnXxxForCubelengthOf108(self): 
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww\
+                        bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx') 
+        
+    def test_check_907_ShouldReturnXxxFor5ColorCube(self): 
+        parm = {'op':'check',
+                'cube':'abcdebbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx')
+        
+    def test_check_908_ShouldReturnXxxFor7ColorCube(self): 
+        parm = {'op':'check',
+                'cube':'abcdefgbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx')
+        
+    def test_check_909_ShouldReturnXxxForOccurrenceNotMatchAtTheBeginning(self): 
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbbrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx')
+        
+    def test_check_910_ShouldReturnXxxForOccurrenceNotMatchAtTheEnd(self): 
+        parm = {'op':'check',
+                'cube':'bbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx')
+        
+    def test_check_911_ShouldReturnXxxForFaceColorNotMatchAtTheBeginning(self): 
+        parm = {'op':'check',
+                'cube':'bbbbrbbbbbrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx')   
+        
+    def test_check_912_ShouldReturnXxxForFaceColorNotMatchAtTheEnd(self): 
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbwrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwbwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx')
+    
+    def test_check_913_ShouldReturnXxxForContradictoryEdge(self): 
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgygggggggoooooooooyyyyyyygywwwwwwwww'}        
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx')
+        
+    def test_check_914_ShouldReturnXxxForContradictoryCorner(self): 
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrryggggggggoooooooooyyyyyygyywwwwwwwww'}        
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: xxx')
 
